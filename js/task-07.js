8ee7570d1fb618b2c813028ba617f45bc5370d7f
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-05-24 00:27:41 +0300

    задача 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,1 +1,14 @@
-// Напиши скрипт, который реагирует на изменение значения input#font-size-control (событие input) и изменяет инлайн-стиль span#text обновляя свойство font-size. В результате при перетаскивании ползунка будет меняться размер текста.
+// Напиши скрипт, который реагирует на изменение значения
+// input#font - size - control(событие input) и изменяет инлайн -
+// стиль span#text обновляя свойство font - size.В результате при
+// перетаскивании ползунка будет меняться размер текста.
+
+const refs = {
+  input: document.querySelector("#font-size-control"),
+  span: document.querySelector("#text"),
+};
+
+refs.input.addEventListener("input", onInputChange);
+function onInputChange(event) {
+  refs.span.style.fontSize = event.target.value + "1px";
+}
