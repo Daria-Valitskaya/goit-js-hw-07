e80195d9412f2d057ad4634d94e665898996f373
Daria-Valitskaya <75114227+Daria-Valitskaya@users.noreply.github.com>
2021-05-24 00:08:35 +0300

    задача 6

--- js/task-06.js
+++ js/task-06.js
@@ -2,3 +2,15 @@
 // Сколько символов должно быть в инпуте, указывается в его атрибуте data-length.
 // Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
+const inputEl = document.querySelector("#validation-input");
+const currentValue = inputEl.getAttribute("data-length");
+inputEl.addEventListener("blur", onInputBlur);
+function onInputBlur(event) {
+  if (event.target.value.length > parseInt(currentValue)) {
+    inputEl.classList.add("invalid");
+    inputEl.classList.remove("valid");
+  } else {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+  }
+}
